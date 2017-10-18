9120477815775007b09db3f6af3cec61c5d9e14c
anarad <anaradilovab@gmail.com>
2017-10-18 16:16:41 +0600

    Update __init__.py

--- widget-workshop/widget/workshop_widget/__init__.py
+++ widget-workshop/widget/workshop_widget/__init__.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals, absolute_import, print_function
 from hubs.widgets.base import Widget
 from hubs.widgets.view import RootWidgetView
 from .bzlib import get_package_reviews
+from hubs.widgets.caching import CachedFunction
 
 class WorkshopWidget(Widget):
 
@@ -15,16 +16,38 @@ class WorkshopWidget(Widget):
         ),
     ]
 
+class GetReviews(CachedFunction):
 
-class BaseView(RootWidgetView):
-
-    def get_context(self, instance, *args, **kwargs):
-        email = instance.config["email"]
+    def execute(self):
+        email = self.instance.config["email"]
         if not email:
             return {"reviews":{}}
 	total=0
 	for val in get_package_reviews(email).values():
-		total+=len(val)
+	    total+=len(val)
 	if total == 0:
-		return {"reviews":{"no_bugs":True}}
+	    return {"reviews":{"no_bugs":True}}
 	return {"reviews": get_package_reviews(email)}
+
+    def should_invalidate(self, message):
+	if ".bugzilla.bug." not in message["topic"]:
+            return False
+	try:
+            product = message['msg']['bug']['product']
+	    component = message['msg']['bug']['component']
+	    reporter = message['msg']['bug']['creator']
+	except KeyError:
+	    return False
+	return (
+	    product == "Fedora" and
+	    component == "Package Review" and
+	    reporter == self.instance.config["email"]
+		)
+
+
+class BaseView(RootWidgetView):
+
+    def get_context(self, instance, *args, **kwargs):
+	get_reviews = GetReviews(instance)
+    	return {"reviews": get_reviews()}
+       
